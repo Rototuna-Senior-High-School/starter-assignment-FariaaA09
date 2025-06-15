26dbd23529b85d06f40e813697ddf6f972dcb916
FariaaA09 <fariaaahmed@rhs.school.nz>
2025-06-16 10:49:12 +1200

    Add files via upload

--- code.py
+++ code.py
@@ -1,25 +1,42 @@
+# Import Pi Pico board so we can talk to the pins
 import board
+
+# Import PWM (Pulse Width Modulation) to dim LEDs
 import pwmio
+
+# Import Analog input to read the potentiometer (knob)
 import analogio
+
+# Import time to add small delays (smooth transitions)
 import time
 
+# The potentiometer is connected to A0
 knob = analogio.AnalogIn(board.A0)
 
-led = pwmio.PWMOut(board.GP3, frequency=1000, duty_cycle=0)
-led2 = pwmio.PWMOut(board.GP0, frequency=1000, duty_cycle=0)
+# Set up two LEDs with PWM (so we can fade them)
+ledred = pwmio.PWMOut(board.GP3, frequency=1000, duty_cycle=0)
+ledblue = pwmio.PWMOut(board.GP0, frequency=1000, duty_cycle=0)
 
+# Loop forever
 while True:
-    knob_value = knob.value  # 0–65535
+# Read the potentiometer value (0 to 65535)
+    knob_value = knob.value
     print("Knob:", knob_value)
 
+# If knob is in the lower half (0–32767), fade in red LED
     if knob_value < 32768:
-        led.duty_cycle = knob_value * 2
-        led2.duty_cycle = 0
-    else:
-        led.duty_cycle = 65535
-        led2_value = (knob_value - 32768) * 2
-        if led2_value > 65535:
-            led2_value = 65535
-        led2.duty_cycle = led2_value
+        ledred.duty_cycle = knob_value * 2
+        ledblue.duty_cycle = 0
 
+# If knob is in the upper half (32768–65535), keep red on and fade in blue
+    else:
+        ledred.duty_cycle = 65535
+        ledblue_value = (knob_value - 32768) * 2
+
+        if ledblue_value > 65535:
+            ledblue_value = 65535
+
+        ledblue.duty_cycle = ledblue_value
+
+# Wait a tiny bit to smooth it out
     time.sleep(0.01)
