5c60e3cea38d17834b17674c1340224c5c25f862
FariaaA09 <fariaaahmed@rhs.school.nz>
2025-06-16 10:30:35 +1200

    petentiometre light brightness

--- code.py
+++ code.py
@@ -1,24 +1,25 @@
-import time
 import board
+import pwmio
 import analogio
-import digitalio
+import time
 
 knob = analogio.AnalogIn(board.A0)
 
-transistor_pin = digitalio.DigitalInOut(board.GP15)
-transistor_pin.direction = digitalio.Direction.OUTPUT
+led = pwmio.PWMOut(board.GP3, frequency=1000, duty_cycle=0)
+led2 = pwmio.PWMOut(board.GP0, frequency=1000, duty_cycle=0)
 
 while True:
-    analog_value = knob.value
+    knob_value = knob.value  # 0–65535
+    print("Knob:", knob_value)
 
-    print('raw = ' + str(knob.value))
+    if knob_value < 32768:
+        led.duty_cycle = knob_value * 2
+        led2.duty_cycle = 0
+    else:
+        led.duty_cycle = 65535
+        led2_value = (knob_value - 32768) * 2
+        if led2_value > 65535:
+            led2_value = 65535
+        led2.duty_cycle = led2_value
 
-    delay_time = analog_value / 20000
-
-    transistor_pin.value = True
-    print(f"LEDs ON for {delay_time:.2f} seceonds")
-    time.sleep(delay_time)
-
-    transistor_pin.value = False
-    print(f"LEDs OFFfor {delay_time:.2f} seceonds")
-    time.sleep(delay_time)
+    time.sleep(0.01)
